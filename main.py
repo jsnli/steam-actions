51eef0df79c62ff0b780382f77dfafba0b79fc02
Jason <130726285+jsnli@users.noreply.github.com>
2024-05-26 00:46:50 -0400

    update fixes for workflow

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
 user = os.getenv("POSTGRES_USER")
-print(user)
+
+if 'POSTGRES_USER' in os.environ:
+    print("User found in environ")
+    user = os.environ['POSTGRES_USER']
+    print(user)

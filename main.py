b52cf9ec9cb6bff4a374c648d03786c0fc739900
Jason <130726285+jsnli@users.noreply.github.com>
2024-05-26 17:03:59 -0400

    build base db

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import psycopg
 import os
+import asyncio
+import json
 from dotenv import load_dotenv
-
+from req import fetch
 
 if os.path.isfile('./.env'):
     load_dotenv()
@@ -11,8 +13,36 @@ host = os.environ['POSTGRES_HOST']
 password = os.environ['POSTGRES_PASSWORD']
 name = os.environ['POSTGRES_DATABASE']
 
-conn = psycopg.connect(f"host={host} dbname={name} user={user} password={password}")
-cur = conn.cursor()
-cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",
-            (200, "two hundred"))
-conn.commit()
+
+async def get_app_list(last_appid=0):
+    app_list = await fetch(last_appid)
+    data = []
+
+    for index, app in enumerate(app_list["apps"]):
+        data.append((app["appid"], app["name"], app["last_modified"]))
+
+    post_app_list(data)
+
+    if "have_more_results" in app_list:
+        await get_app_list(app_list["last_appid"])
+
+
+def post_app_list(data):
+    conn = psycopg.connect(f"host={host} dbname={name} user={user} password={password}")
+    cur = conn.cursor()
+
+    query = """
+        INSERT INTO Games (AppID, Name, LastModified) VALUES (%s, %s, %s)
+    """
+
+    cur.executemany(query, data)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+async def main():
+    await get_app_list()
+
+asyncio.run(main())
---
+++ req.py
@@ -0,0 +1,20 @@
+import asyncio
+import json
+import os
+import aiohttp
+from dotenv import load_dotenv
+
+if os.path.isfile('./.env'):
+    load_dotenv()
+
+base = "https://api.steampowered.com/IStoreService/GetAppList/v1/?"
+key = os.environ['STEAM_KEY']
+url = f"{base}key={key}&include_games=true&max_results=50000"
+
+
+async def fetch(last_appid=0):
+    print(f"fetching: {last_appid}")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url + f"&last_appid={last_appid}") as response:
+            result = await response.json()
+            return result["response"]
